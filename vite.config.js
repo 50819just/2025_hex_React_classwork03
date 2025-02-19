44fbda86e3881b8271e1ffd1501392991579fbd9
50819just <justin50819just@gmail.com>
2025-02-19 17:29:35 +0800

    1140219-2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //開發中、產品路徑
+  base: process.env.NODE_ENV === "production" ? '/2025_hex_React_classwork03/' : '/' ,
   plugins: [react()],
 })
